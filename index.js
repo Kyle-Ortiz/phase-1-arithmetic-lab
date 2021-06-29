8cb1321684ff69d0f698829c023e3061c068b3d0
Kyle Ortiz <kyleaortiz@gmail.com>
2021-06-29 11:29:28 -0500

    commit tree aee582d390b0c8757eb562606aa19b71b3e146cc

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+const num3 = 24;
+const num4 = 5;
+let mod = num3 % num4;
+
+const max = Math.max(15, 16, 17, 18, 19, 20);
